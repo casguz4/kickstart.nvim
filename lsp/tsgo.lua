493e9301bc5f11954e5909dc3ab532d6cfe2694a
wewantwebsites <cas.guzdziol@wewantweb.site>
2025-04-27 17:28:47 -0400

    setup tsgo lspconfig

--- lsp/tsgo.lua
+++ lsp/tsgo.lua
@@ -1,5 +1,11 @@
 --- https://gist.github.com/kr-alt/24aaf4bad50d603c3c6a270502e57209
-local root_files = { 'tsconfig.base.json', 'tsconfig.json', 'jsconfig.json', 'package.json', '.git' }
+local root_files = {
+  'tsconfig.base.json',
+  'tsconfig.json',
+  'jsconfig.json',
+  'package.json',
+  '.git',
+}
 local paths = vim.fs.find(root_files, { stop = vim.env.HOME })
 local root_dir = vim.fs.dirname(paths[1])
 
@@ -11,15 +17,17 @@ end
 ---@type vim.lsp.Config
 local M = {}
 
-M.cmd = { os.getenv 'TSGOPATH', 'lsp', '--stdio' }
+M.cmd = {
+  vim.env.TSGOPATH .. '/built/local/tsgo',
+  'lsp',
+  '--stdio',
+}
 
 M.filetypes = {
   'javascript',
   'javascriptreact',
-  'javascript.jsx',
   'typescript',
   'typescriptreact',
-  'typescript.tsx',
 }
 
 M.root_dir = root_dir
--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -7,7 +7,7 @@ return {
       vim.api.nvim_set_keymap('i', '<C-J>', 'copilot#Accept("<CR>")', { silent = true, expr = true })
 
       -- Enable Copilot
-      vim.cmd 'Copilot enable'
+      -- vim.cmd 'Copilot enable'
     end,
   },
   {
