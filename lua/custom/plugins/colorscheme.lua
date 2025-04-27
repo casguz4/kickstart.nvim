d580e6fd14ea857627958a120c887b10deded221
Stephan Randle <stephanrandle@hotmail.com>
2025-04-27 15:34:41 -0400

    feat: enable native lsp in cas config
    * feat: enable native lsp in cas config
    
    * chore: added catppuccin option typing and removed hallucinated blink function (theme flash)

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -2,7 +2,9 @@ return {
   'catppuccin/nvim',
   name = 'catppuccin',
   priority = 1000, -- load before eveything else
-  config = function()
-    vim.cmd.colorscheme 'catppuccin'
-  end,
+  ---@module 'catppuccin'
+  ---@type CatppuccinOptions
+  opts = {
+    transparent_background = true,
+  },
 }
